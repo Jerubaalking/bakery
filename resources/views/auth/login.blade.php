5c25811d563fc77dfded7cc2ea79b09946e497d9
jerubaalking <gsainyeye@gmail.com>
2024-10-09 17:26:22 +0300

    added bakery site

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -34,8 +34,7 @@
                                 <p class="lead">User Login </p>
                             </div>
                             <form class="form-auth-small" method="POST" action="{{ route('login') }}">
-                                <!-- @csrf -->
-                                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                @csrf
                                 <div class="form-group">
                                     <label for="signin-email" class="control-label sr-only">Email</label>
                                     <input type="email" name="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" id="signin-email" value="{{ old('email') }}" autofocus required placeholder="Email">
--- routes/web.php
+++ routes/web.php
@@ -191,6 +191,9 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/', function () {
+	return view('welcome');
+});
+Route::get('/login', function () {
 	return view('auth.login');
 });
 
